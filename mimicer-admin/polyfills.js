f785a2023292caabf8c8676d2e737db15d3c8dc9
Pham Quang Thai <binlambaobao142@gmail.com>
2025-01-09 01:19:15 +0700

    add dockerfile to admin

---
+++ mimicer-admin/polyfills.js
@@ -0,0 +1,3 @@
+if (typeof structuredClone === 'undefined') {
+    global.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
+}
--- mimicer-admin/vite.config.ts
+++ mimicer-admin/vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import './polyfills'
+import path from 'path';
+
+console.log('Vite configuration loaded');
+console.log('Current directory:', __dirname);
+console.log('Resolved dist directory:', path.resolve(__dirname, 'dist'));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        dir: path.resolve(__dirname, 'dist'),
+        entryFileNames: 'bundle.js',
+        format: 'iife',
+      }
+    }
+  }
 })
